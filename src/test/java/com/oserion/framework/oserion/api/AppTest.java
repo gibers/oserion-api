8f639ab78c6a742c70fc04f72a0b0480a93152a7
gibers <chbaptis@hotmail.fr>
2016-01-02 18:50:56 +0100

    try resolve conflicts

--- src/test/java/com/oserion/framework/oserion/api/AppTest.java
+++ src/test/java/com/oserion/framework/oserion/api/AppTest.java
@@ -31,24 +31,24 @@ import com.oserion.framework.api.util.Supervision;
 @ContextConfiguration(classes=OserionBuilder.class)
 public class AppTest {
 
-    public static final String PROPERTY_CONFIG_PATH = "oserion.config.path";
-    
-    private File f1 = new File("C:\\Users\\Jean-Baptiste\\Documents\\oserion\\fichierTestHtml1.html"); 
-    
+	public static final String PROPERTY_CONFIG_PATH = "oserion.config.path";
+
+	private File f1 = new File("C:\\Users\\Jean-Baptiste\\Documents\\oserion\\fichierTestHtml1.html"); 
+
 	@Autowired
 	private Api418Facade a418f;
-	
+
 	@Autowired
 	private Supervision supervision;
-	
+
 	@BeforeClass
-    public static void avantTests() {
-		
+	public static void avantTests() {
+
 		// Si lancement avec mvn, on sort, car les variables systèmes sont déjà initialisées.
 		if(System.getProperty("skipTests") != null) return;
-        System.out.println("------------------------");
-        System.out.println("Avant Tests");
-        System.out.println("------------------------");
+		System.out.println("------------------------");
+		System.out.println("Avant Tests");
+		System.out.println("------------------------");
 		FileInputStream configFile;
 		try {
 			configFile = new FileInputStream(System.getProperty(AppTest.PROPERTY_CONFIG_PATH));
@@ -59,26 +59,26 @@ public class AppTest {
 			e.printStackTrace();
 		} 
 		System.out.println(" ===> " + System.getProperty("database.host"));
-    }
-	
-//	@Test
-//	public void testInsertOrUpdateTemplate() {
-//		String strTemplate = null;
-//		try {
-//			strTemplate = FileUtils.readFileToString(f1);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		String templateName = FilenameUtils.removeExtension( f1.getName());
-//  
-//		a418f.insertTemplate(templateName , strTemplate);
-//		
-//		System.out.println("adresse mémoire => " + a418f );
-//		assertTrue(true);
-//	}
-	
-	
-//	@Test 
+	}
+
+	//	@Test
+	//	public void testInsertOrUpdateTemplate() {
+	//		String strTemplate = null;
+	//		try {
+	//			strTemplate = FileUtils.readFileToString(f1);
+	//		} catch (IOException e) {
+	//			e.printStackTrace();
+	//		}
+	//		String templateName = FilenameUtils.removeExtension( f1.getName());
+	//  
+	//		a418f.insertTemplate(templateName , strTemplate);
+	//		
+	//		System.out.println("adresse mémoire => " + a418f );
+	//		assertTrue(true);
+	//	}
+
+
+	//	@Test 
 	public void updateTemplate () {
 		String strTemplate = null;
 		try {
@@ -89,44 +89,46 @@ public class AppTest {
 		String templateName = FilenameUtils.removeExtension( f1.getName());
 		a418f.updateTemplate(templateName , strTemplate);
 	}
-	
-	
-//	@Test
+
+
+	//	@Test
 	public void addPageUrl() {
 		String templateName = FilenameUtils.removeExtension( f1.getName());
 		a418f.addPageUrl(templateName, "/toto/titi");
 	}
-	
+
 	@Test
 	public void selectTemplate() {
 		List<ITemplate> listITemplate = a418f.selectTemplates();
 		for( ITemplate it : listITemplate) {
 			System.out.println("nom TEMPLATE : " + it.getName());
-			
+
 			if(it.getListPage() != null) {
-					
+
 				for(IPageReference p1 : it.getListPage()) {
-					
-					if( p1 instanceof PageReference ) {
-						System.out.println("p1 est une pageReference");
-						((PageReference) p1).getTemplate();
-					}
+
+					if(it.getListPage() != null)
+						if( p1 instanceof PageReference ) {
+							System.out.println("p1 est une pageReference");
+							((PageReference) p1).getTemplate();
+							System.out.println(" ** URL : " + p1.getUrl() );
+							System.out.println(" ** KEY : " + p1.getKey() );
+						}
 				}
-				
 			}
 		}
 	}
-	
 
-//	@Test
+
+	//	@Test
 	public void testSupervision() {
 		String templateName = FilenameUtils.removeExtension( f1.getName());
-		
-//		supervision.listContentElementFromTemplateName(templateName);
+
+		//		supervision.listContentElementFromTemplateName(templateName);
 		supervision.listPageFromTemplateName(templateName);
 	}
-	
-	
-	
+
+
+
 }
 
